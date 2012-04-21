d77cba7830fa43a843dfb2d24f7ff909f1492383
Greg Meiste <greg.meiste@gmail.com>
2012-04-21 09:43:04 -0500

    Add empty SuggestServlet to allow app development to continue
    Signed-off-by: Greg Meiste <greg.meiste@gmail.com>

---
+++ src/com/meiste/greg/ptwgame/SuggestServlet.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Gregory S. Meiste  <http://gregmeiste.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.meiste.greg.ptwgame;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class SuggestServlet extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().print(req.getReader().readLine());
+    }
+}
